c4e6cf8b2038ee6ab00db6336724054df1542e34
keerthi-automation-2025 <keertiprabhu1@gmail.com>
2026-03-10 21:45:04 +0530

    New Automation framework

--- playwright1.config.js
+++ playwright1.config.js
@@ -12,16 +12,31 @@ const config = {
   
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  projects: [
+    {
+      name:'chrome',
   use: {
 
     browserName : 'chromium',
     headless : false,
+    viewport : {width: 720, height: 720},
     screenshot : 'on',
     trace : 'on',//off,on
-    
-    
-    
+    },
   },
+  {
+    name:'webkitprofile',
+use: {
+
+  browserName : 'webkit',
+  headless : false,
+  screenshot : 'on',
+  trace : 'on',//off,on
+  ...devices['iPhone 11']
+  },
+}
+]
 
 
 };
